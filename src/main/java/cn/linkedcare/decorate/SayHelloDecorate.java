af4fe16556b0ea54d7409cd8d94c0114a87361a9
benji <zh1108110162@163.com>
2018-05-02 16:16:38 +0800

    修改装饰着设计模式的BUG

--- src/main/java/cn/linkedcare/decorate/SayHelloDecorate.java
+++ src/main/java/cn/linkedcare/decorate/SayHelloDecorate.java
@@ -3,13 +3,14 @@ package cn.linkedcare.decorate;
 /**
  * Created by Benji on 2018/5/2.
  */
-public abstract class SayHelloDecorate {
+public abstract class SayHelloDecorate implements SayHello {
     private final SayHello sayHello;
 
     public SayHelloDecorate(SayHello sayHello) {
         this.sayHello = sayHello;
     }
 
+    @Override
     public void sayHello() {
         sayHello.sayHello();
     }
